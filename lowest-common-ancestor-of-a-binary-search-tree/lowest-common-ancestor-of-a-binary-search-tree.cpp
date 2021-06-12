bfe1605e957bd1f58344f15d4d46928b75a2388e
GopinathBalaji <70364682+GopinathBalaji@users.noreply.github.com>
2021-06-12 10:15:12 +0530

    Time: 20 ms (99.40%), Space: 23.2 MB (43.20%) - LeetHub

--- lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -12,42 +12,62 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         
-        vector<int> pathv1;
-        vector<int> pathv2;
-       bool a1 = path(root,pathv1,p); 
-       bool a2 = path(root,pathv2,q);
-           
-        int i;
-        for(i=0;i<pathv1.size() && i < pathv2.size();i++){
-            if(pathv1[i] != pathv2[i]){
-                break;
-            }
-        }
-        int a = pathv1[i-1];
-         TreeNode* node = new TreeNode(a);
-        node->val = a;
-        node->left = NULL;
-        node->right = NULL;
-        
-        return node;
-    }
-    
-    bool path(TreeNode* root,vector<int> &vec,TreeNode* node){
         if(root==NULL){
-            return false;
+            return NULL;
+        }
+        if(root->val == p->val || root->val == q->val){
+            return root;
         }
         
-        vec.push_back(root->val);
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
         
-        if(root->val == node->val){
-            return true;
+        if(left && right){
+            return root;
         }
         
-        if((root->left && path(root->left,vec,node)) || (root->right && path(root->right,vec,node))){
-            return true;
+       else if(left){
+            return left;
         }
+        return right;
         
-       vec.pop_back();
-       return false;
-    }
+        
+        
+        
+        
+//         vector<int> pathv1;
+//         vector<int> pathv2;
+//        bool a1 = path(root,pathv1,p); 
+//        bool a2 = path(root,pathv2,q);
+           
+//         int i;
+//         for(i=0;i<pathv1.size() && i < pathv2.size();i++){
+//             if(pathv1[i] != pathv2[i]){
+//                 break;
+//             }
+//         }
+//         int a = pathv1[i-1];
+//          TreeNode* node = new TreeNode(a);
+  
+//         return node;
+//     }
+    
+//     bool path(TreeNode* root,vector<int> &vec,TreeNode* node){
+//         if(root==NULL){
+//             return false;
+//         }
+        
+//         vec.push_back(root->val);
+        
+//         if(root->val == node->val){
+//             return true;
+//         }
+        
+//         if((root->left && path(root->left,vec,node)) || (root->right && path(root->right,vec,node))){
+//             return true;
+//         }
+        
+//        vec.pop_back();
+//        return false;
+     }
 };
