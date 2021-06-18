fae0a2aad5481271fc066a9cece51bac04e16415
GopinathBalaji <70364682+GopinathBalaji@users.noreply.github.com>
2021-06-18 19:00:55 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (58.12%) - LeetHub

--- nim-game/nim-game.cpp
+++ nim-game/nim-game.cpp
@@ -4,3 +4,22 @@ public:
        return n%4!=0;
     }
 };
+
+/*
+// DP Solution JAVA
+public boolean canWinNim(int n) {
+List list = new LinkedList();
+for(int i=1; i <= n; i++){
+if (i <= 3) list.add(true);
+else {
+boolean res = false;
+res |= !list.get(0);
+res |= !list.get(1);
+res |= !list.get(2);
+list.remove(0);
+list.add(res);
+}
+}
+return list.remove(2);
+}
+*/
