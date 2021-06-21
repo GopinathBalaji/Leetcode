f2faa6b159daaabba353ae943532ee65d957780f
GopinathBalaji <70364682+GopinathBalaji@users.noreply.github.com>
2021-06-21 10:11:02 +0530

    Time: 768 ms (5.11%), Space: 10 MB (61.65%) - LeetHub

---
+++ two-sum/two-sum.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    
+// Method 1:   Brute force approach: 
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums.size();j++){
+                if(i!=j && nums[i]+nums[j]==target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    return ans;
+                }
+            }
+        }
+        return {};
+    }
+};
+
+// Method 2:  Two pass Hash map
+      /*
+      public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        map.put(nums[i], i);
+    }
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement) && map.get(complement) != i) {
+            return new int[] { i, map.get(complement) };
+        }
+    }
+    throw new IllegalArgumentException("No two sum solution");
+}
+      */
+
+
+
+// Method 3:   Single pass Hash Map
+      /*
+      public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement)) {
+            return new int[] { map.get(complement), i };
+        }
+        map.put(nums[i], i);
+    }
+    throw new IllegalArgumentException("No two sum solution");
+}
+      */
