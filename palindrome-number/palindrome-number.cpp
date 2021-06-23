241b6127f7300882b05558f0d74a1c3a697970a3
GopinathBalaji <70364682+GopinathBalaji@users.noreply.github.com>
2021-06-23 09:41:27 +0530

    Time: 16 ms (34.69%), Space: 6 MB (25.86%) - LeetHub

--- palindrome-number/palindrome-number.cpp
+++ palindrome-number/palindrome-number.cpp
@@ -19,43 +19,29 @@ public:
     }
 };
 
-// Method 2: Similar approach but without string
+// Method 2: Without using extra space
     /*
-    class Solution
-{
-public:
-bool isPalindrome(int x)
-{
-vector a;
-int q;
-if (x < 0)
-{
-return false;
+   public class Solution {
+    public bool IsPalindrome(int x) {
+        // Special cases:
+        // As discussed above, when x < 0, x is not a palindrome.
+        // Also if the last digit of the number is 0, in order to be a palindrome,
+        // the first digit of the number also needs to be 0.
+        // Only 0 satisfy this property.
+        if(x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int revertedNumber = 0;
+        while(x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+        // When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
+        // For example when the input is 12321, at the end of the while loop we get x = 12, revertedNumber = 123,
+        // since the middle digit doesn't matter in palidrome(it will always equal to itself), we can simply get rid of it.
+        return x == revertedNumber || x == revertedNumber/10;
+    }
 }
-else
-{
-while (x != 0)
-{
-q = x % 10;
-a.push_back(q);
-x = x / 10;
-}
-int i = 0;
-int j = a.size() - 1;
-while (i < j)
-{
-if (a[i] == a[j])
-{
-i++;
-j--;
-}
-else
-{
-return false;
-}
-}
-}
-return true;
-}
-};
     */
