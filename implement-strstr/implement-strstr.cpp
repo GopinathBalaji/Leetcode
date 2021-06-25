b0d8169ccb4f947bde2bda761f038a7ed236c855
GopinathBalaji <70364682+GopinathBalaji@users.noreply.github.com>
2021-06-25 12:15:07 +0530

    Time: 4 ms (77.00%), Space: 6.8 MB (72.20%) - LeetHub

---
+++ implement-strstr/implement-strstr.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+//         Method 1: Two pointer
+        
+        if(needle.length()==0){
+            return 0;
+        }
+       int j = 0;
+        
+        for(int i=0;i<haystack.length();i++){
+           if(haystack[i]==needle[j]){
+               j++;
+           }else{
+               i -= j;
+               j=0;
+           }       
+            if(j==needle.length()){
+                return i-j+1;
+            }
+        }
+    return -1;
+    }
+};
