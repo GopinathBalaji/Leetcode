3dbb3a2b41adbaf6bd166da6352b8a9d4096186e
GopinathBalaji <70364682+GopinathBalaji@users.noreply.github.com>
2021-07-27 20:21:18 +0530

    Time: 48 ms (28.82%), Space: 14.2 MB (81.25%) - LeetHub

--- minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -62,3 +62,39 @@ public:
          return dp[n] > n+1 ? -1 : dp[n];
      }
    */
+
+// Method 3: Similar to question "Jump Game 2"
+   /*
+    ublic:
+    int minTaps(int n, vector<int>& ranges) {
+        vector<int> jumps(n+1, 0);
+        
+        for(int i = 0; i<n+1; i++) {
+            int l = max(0, i-ranges[i]);
+            int r = min(n, i+ranges[i]);
+            
+            jumps[l] = max(jumps[l], r-l); //from l, that's farthest I can jump to right
+        }
+        
+        //Now this questions has turned to Jump Game II
+        //Where you have maximum jump you can take form an index i and
+        //you have to reach end
+        
+        //This is just Jump Game - II code
+        int currEnd  = 0;
+        int maxReach = 0;
+        int count    = 0;
+        for(int i = 0; i<n; i++) {
+            maxReach = max(maxReach, jumps[i]+i);
+            
+            if(i == currEnd) {
+                count++;
+                currEnd = maxReach;
+            }
+        }
+        
+        if(currEnd >= n)
+            return count;
+        return -1;
+    }
+   */
