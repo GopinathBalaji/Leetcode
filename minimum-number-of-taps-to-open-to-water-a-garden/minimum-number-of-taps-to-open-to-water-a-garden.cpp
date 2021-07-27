2a26b326d5d40b79de9969ba698c2dfbe76d1756
GopinathBalaji <70364682+GopinathBalaji@users.noreply.github.com>
2021-07-27 12:30:31 +0530

    Time: 60 ms (15.54%), Space: 14 MB (97.95%) - LeetHub

---
+++ minimum-number-of-taps-to-open-to-water-a-garden/minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+//     Method 1: Brute Force
+    int minTaps(int n, vector<int>& ranges) {
+        int  min = 0;
+        int max = 0;
+        int count = 0;
+        
+//         when max is >= n that means we have covered the entire garden
+        while(max<n){
+            
+            //Choose the tap with maximum range it can reach to right
+            //For a given min value (to left)
+            for(int i=0;i<ranges.size();i++){
+                if(i-ranges[i]<=min && i+ranges[i]>max){
+                    max = i+ranges[i];
+                }
+            }
+            
+            //It means we couldn't expand our range to right
+            if(max==min){
+                return -1;
+            }
+            
+             //Now, we have reached till max, next we want to cover more than this max
+            //starting from min
+            min = max;
+            count++;
+        }
+        return count;
+    }
+};
+
+// Method 2: Bottom Up Dynamic Programming
+   /*
+     int minTaps(int n, vector<int>& ranges) {
+         vector<int> dp(ranges.size(),ranges.size()+1);
+         
+        //t[i] = minimum taps needed to cover 0 to ith of Garden
+         dp[0] = 0;  //I need 0 tap to cover 0 to 0
+         
+         //Why start from i = 0
+        //Because value at ranges[0] also impacts the range {i-ranges[i], i+ranges[i]}
+        //We will miss that impact if we skip i = 0
+         for(int i=0;i<ranges.size();i++){
+             int left = max(0,i-ranges[i]);
+             int right = min(n, i+ranges[i]);
+             
+             
+        //     0 to l is watered
+        //    We now need to find minimum taps to cover from (l+1) to r
+             for(int j=left+1;j<=right;j++){  
+             
+             //Check if this range from(left+1..right) can
+            //  be watered using less number of taps than previous
+                 dp[j] = min(dp[j],dp[j]+1);
+             }
+         }
+         
+         //if min taps needed to water ground is greater than (n+1) we return -1
+        //Because we only had (n+1) taps
+         return dp[n] > n+1 ? -1 : dp[n];
+     }
+   */
