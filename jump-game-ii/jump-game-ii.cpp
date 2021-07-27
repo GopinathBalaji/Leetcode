1e94d77993171b8eeb9869f448b058c847a9d53e
GopinathBalaji <70364682+GopinathBalaji@users.noreply.github.com>
2021-07-27 22:20:45 +0530

    Time: 8 ms (97.64%), Space: 16.3 MB (87.32%) - LeetHub

--- jump-game-ii/jump-game-ii.cpp
+++ jump-game-ii/jump-game-ii.cpp
@@ -5,27 +5,6 @@ public:
 //     Similar questions ( Remove Covered Intervals, Minimum Number of Arrows to Burst Balloons, 
 //    and Jump Game II.)
     
-//     int jump(vector<int>& nums) {
-//         if(nums.size()==1){
-//             return 0;
-//         }
-        
-//         int maxreach = 0 + nums[0];
-//         int i=0,k=0,step=1;
-        
-//         while(maxreach < nums.size()-1){
-//             step++;
-//             for(int j=i+1;j<nums[i]+i;j++){
-//                 if(nums[j] + j > maxreach){
-//                     maxreach = nums[j] + j;
-//                     k = j;
-//                 }
-//             }
-//             i = k;
-//         }
-//         return step;
-//     }
-    
     int jump(vector<int>& a)
 {
 	if (a.size() == 1) return 0;
@@ -45,4 +24,26 @@ public:
 	}
 	return step;
 }
+    
+//     ---------------OR-------------------
+//     Similar approach
+// we keep two pointers start and end that record the current range of the starting nodes. Each time 
+// after we make a move, update start to be end + 1 and end to be the farthest index that can be reached
+// in 1 move from the current [start, end].
+    /*
+      int jump(vector<int>& nums) {
+        int n = nums.size(), step = 0, start = 0, end = 0;
+        while (end < n - 1) {
+            step++; 
+			int maxend = end + 1;
+			for (int i = start; i <= end; i++) {
+                if (i + nums[i] >= n - 1) return step;
+				maxend = max(maxend, i + nums[i]);
+			}
+            start = end + 1;
+            end = maxend;
+        }
+		return step;
+    }
+    */
 };
