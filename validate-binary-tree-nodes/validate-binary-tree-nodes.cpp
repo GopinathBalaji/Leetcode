61dbc21a3cdaf3ae7b3b49dc88f575a2bcfcba86
GopinathBalaji <70364682+GopinathBalaji@users.noreply.github.com>
2021-08-06 19:14:47 +0530

    Time: 36 ms (93.22%), Space: 32.8 MB (53.64%) - LeetHub

--- validate-binary-tree-nodes/validate-binary-tree-nodes.cpp
+++ validate-binary-tree-nodes/validate-binary-tree-nodes.cpp
@@ -68,6 +68,73 @@ public:
 };
 
 
+// Method 2: Union Find
+   /*
+    class Solution {
+public:
+    vector<int> parent,size;
+
+    int find(int i){
+        while(parent[i]!=i){
+            parent[i]=parent[parent[i]];
+            i=parent[i];
+        }
+        return i;
+    }
+    void union_(int i,int j){
+            parent[j]=i;
+    }
+    bool validateBinaryTreeNodes(int n, vector<int>& leftchild, vector<int>& rightchild) {
+        vector<int> node_parent(n),vis(n,0);   //keeps count of parent
+		//if any node has more than 1 parent return false
+        for(int i=0;i<n;i++){
+            if(leftchild[i]!=-1 ){
+                node_parent[leftchild[i]]++;
+                if(node_parent[leftchild[i]]>1)return false;
+            }
+            if(rightchild[i]!=-1){
+                node_parent[rightchild[i]]++;
+                if(node_parent[rightchild[i]]>1) return false;
+            }
+        }
+        //checking for possible root
+        int possibleroot=-1,count=0;
+        for(int i=0;i<n;i++){
+            if(node_parent[i]==0){
+                possibleroot=i;
+                count++;
+                if(count>1)return false;  //if more than 2 root -> false
+            }
+        }
+        if(possibleroot==-1)return false;   //no possible root
+        //find connected component  dsu and check if only component is there so all nodes are connected
+        parent.resize(n);size.resize(n,1);
+        for(int i=0;i<n;i++)parent[i]=i;
+        for(int i=0;i<n;i++){
+            int u=i,v=leftchild[i],w=rightchild[i];
+            int x=find(u);
+            if(v!=-1){
+                int y=find(v);
+                if(x!=y)union_(x,y);
+            }
+            if(w!=-1){
+                int z=find(w);
+                if(x!=w)union_(x,w);
+            }
+            
+        }
+        int connectedcomponent=0;
+        count=0;
+        for(int i=0;i<n;i++){
+            if (parent[i]==i){cout<<parent[i]<<" ";count++;}
+        }
+        if (count>1)return false;
+        return true;
+        
+    }
+};
+   */
+
 
 
 
