c9191ebd7a2f14c1de3a6b97fc7ea787bf4336be
GopinathBalaji <70364682+GopinathBalaji@users.noreply.github.com>
2021-08-09 19:22:23 +0530

    Time: 244 ms (78.24%), Space: 85.4 MB (62.43%) - LeetHub

--- reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -4,7 +4,7 @@ public:
 //         Simple DFS from Source to Destination won't work because the question asks for MINIMUM number 
 //           of edges changed. 
 // We put outward facing edges in one vector, and keep the reverse in another.
-// Starting from the city, we switch edges that are facing away from us.
+// Starting from the city, we count edges that are facing away from us.
 
 // If there is a node that faces inward to us that we haven't visited yet, it would be in our back vector.
 // We need to add inward facing nodes to the queue as well, since they might have neighbors that need 
@@ -45,3 +45,31 @@ public:
         return ans;
     }
 };     
+
+// Method 2: DFS
+//       Procedure is similar to the previous approach
+   /*
+    int dfs(int n,int num,vector<int>out[],vector<int>in[],vector<bool>& vis){
+        int count=0;
+        vis[num]=true;
+        for(int i=0;i<out[num].size();i++){
+            if(!vis[out[num][i]])
+                count+=1+dfs(n,out[num][i],out,in,vis);
+        }
+        for(int i=0;i<in[num].size();i++){
+            if(!vis[in[num][i]])
+                count+=dfs(n,in[num][i],out,in,vis);
+        }
+        return count;
+    }
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<int>out[n];
+        vector<int>in[n];
+        vector<bool>vis(n,false);
+        for(int i=0;i<n-1;i++){
+            out[connections[i][0]].push_back(connections[i][1]);
+            in[connections[i][1]].push_back(connections[i][0]);
+        }
+        return dfs(n,0,out,in,vis);
+    }
+   */
