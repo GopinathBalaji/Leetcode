8994e0136c551bcb204c14793bcca3e247d6be53
GopinathBalaji <70364682+GopinathBalaji@users.noreply.github.com>
2025-09-02 20:42:50 -0500

    Time: 2 ms (73.85%) | Memory: 53.5 MB (21.21%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,47 +1,27 @@
+// Boyer–Moore Voting Algorithm
+/*
+Think of each element as a vote. We keep a running candidate and a count (its lead).
+
+When we see the candidate again, we increment (lead grows).
+When we see a different number, we decrement (they cancel each other).
+
+If the lead drops to 0, we discard the current candidate (it’s been “canceled out”) and start fresh with the next element.
+
+Because a true majority element appears more than n/2 times, it cannot be completely canceled by all other elements; it’s guaranteed to be the final candidate.
+*/
 class Solution {
     public int majorityElement(int[] nums) {
-    //     HashMap<Integer,Integer> map = new HashMap<>();
-    //     for(int num: nums){
-    //         if(map.containsKey(num)){
-    //             map.put(num, map.get(num)+1);
-    //         }
-    //         else{
-    //             map.put(num, 1);
-    //         }
-    //     }
+        int candidate = 0;
+        int count = 0;
 
-    //     int maximum = 0;
-    //     int maxele = nums[0];
+        for(int x: nums){
+            if(count == 0){
+                candidate = x;
+            }
 
-    //    for(int num: nums){
-    //     if (map.get(num) > maximum) {
-    //             maxele = num;
-    //             maximum = map.get(num);
-    //         }
-    //    }
-
-    //    return maxele; 
-
-
-
-    // Moore Voting Algorithm
-    int count = 0;
-    int candidate = 0;
-
-    for(int num: nums){
-        if(count == 0){
-            candidate = num;
+            count += (x == candidate) ? 1 : -1;
         }
 
-        if(num == candidate){
-            count++;
-        }
-        else{
-            count--;
-        }
-    }
-
-    return candidate;
-
+        return candidate;
     }
 }
