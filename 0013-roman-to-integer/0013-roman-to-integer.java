a86edd7c23e888474079b296fd2cab56196148e6
GopinathBalaji <70364682+GopinathBalaji@users.noreply.github.com>
2025-09-07 14:33:51 -0500

    Time: 5 ms (56.01%), Space: 44.9 MB (51.66%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,34 +1,59 @@
+// My answer with extra space allocation for HashMap
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-        
-        int val = 0;
-        int prevVal = 0;
+        Map<Character, Integer> map = Map.of(
+            'I', 1,
+            'V', 5,
+            'X', 10,
+            'L', 50,
+            'C', 100,
+            'D', 500,
+            'M', 1000
+        );
+
+        int ans = 0;
+        int prev = 0;
+
         for(int i=s.length()-1; i>=0; i--){
-            if(i == s.length()-1){
-                val += map.get(s.charAt(i));
-                prevVal = val;
+            int val = map.get(s.charAt(i));
+            if(val < prev){
+                ans -= val;
+                prev = val;
             }else{
-                if(prevVal == map.get(s.charAt(i))){
-                    val += map.get(s.charAt(i));
-                    prevVal = map.get(s.charAt(i));
-                }else if(prevVal > map.get(s.charAt(i))){
-                    val -= map.get(s.charAt(i));
-                    prevVal = map.get(s.charAt(i));                    
-                }else{
-                    val += map.get(s.charAt(i));
-                    prevVal = map.get(s.charAt(i));                    
-                }
+                ans += val;
+                prev = val;
             }
         }
-
-        return val;
+        
+        return ans;
     }
 }
+
+
+
+
+// Same logic but using "switch" to avoid any allocation
+// class Solution {
+//     public int romanToInt(String s) {
+//         int ans = 0, prev = 0; // prev = value to the right
+//         for (int i = s.length() - 1; i >= 0; i--) {
+//             int v = val(s.charAt(i));
+//             ans += (v < prev) ? -v : v;
+//             prev = v;
+//         }
+//         return ans;
+//     }
+
+//     private int val(char c) {
+//         switch (c) {
+//             case 'I': return 1;
+//             case 'V': return 5;
+//             case 'X': return 10;
+//             case 'L': return 50;
+//             case 'C': return 100;
+//             case 'D': return 500;
+//             case 'M': return 1000;
+//             default:  return 0; 
+//         }
+//     }
+// }
