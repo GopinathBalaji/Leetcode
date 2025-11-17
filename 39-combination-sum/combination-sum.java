1f19c6d6ae089486fa79f43ee29f5db11effc03f
GopinathBalaji <70364682+GopinathBalaji@users.noreply.github.com>
2025-11-17 05:07:53 -0600

    Time: 2 ms (87.93%) | Memory: 45.5 MB (20.67%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,4 +1,4 @@
-// Use Backtracking with deduplication 
+// Method 1: Backtracking
 /*
 To Depulicate:
 Establish order and only use candidate numbers whose 
@@ -11,62 +11,34 @@ combination (per problem rules), you pass the same i again.
 */
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<Integer> candi = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
 
-        backtrack(candidates, target, 0, new ArrayList<>(), ans, 0);
-
+        backtrack(candidates, target, candi, ans, 0, 0);
+        
         return ans;
     }
 
-    public void backtrack(int[] candidates, int target, int sum, List<Integer> values, List<List<Integer>> ans, int startIndex){
-        if(sum == target){
-            ans.add(new ArrayList<>(values));
-
+    public void backtrack(int[] candidates, int target, List<Integer> candi, List<List<Integer>> ans, int sum, int idx){
+        if(sum > target || idx >= candidates.length){
+            return;
+        }
+        if(sum == target && idx < candidates.length){
+            ans.add(new ArrayList<>(candi));
             return;
         }
 
 
-        for(int i=startIndex; i<candidates.length; i++){
-            if(sum + candidates[i] <= target){
-                values.add(candidates[i]);
-                backtrack(candidates, target, sum + candidates[i], values, ans, i);
-                values.remove(values.size() - 1);
-            }else{
-                continue;
-            }
+        for(int i=idx; i<candidates.length; i++){
+            candi.add(candidates[i]);
+            sum += candidates[i];
+
+            backtrack(candidates, target, candi, ans, sum, i);
+
+            candi.remove(candi.size() - 1);
+            sum -= candidates[i];
         }
+
+        return;
     }
 }
-
-
-
-// Method 2: Same backtracking with depulication but a different method
-
-// class Solution {
-//     public void dfs(List<List<Integer>> res, List<Integer> path, int currsum, int startidx, int[] candidates, int target){
-//         if(currsum == target){
-//             res.add(new ArrayList<>(path));
-//             return;
-//         }
-
-//         if(currsum < target){
-//             for(int i=startidx;i<candidates.length;i++){
-//                 currsum += candidates[i];
-//                 if(currsum > target){
-//                     break;
-//                 }
-//                 path.add(candidates[i]);
-//                 dfs(res,path,currsum,i,candidates,target);
-//                 path.remove(path.size() - 1);
-//                 currsum -= candidates[i];
-//             }
-//         }
-//     }
-
-//     public List<List<Integer>> combinationSum(int[] candidates, int target){
-//         List<List<Integer>> res = new ArrayList<>();
-//         Arrays.sort(candidates);
-//         dfs(res, new ArrayList<>(), 0, 0, candidates, target);
-//         return res;
-//     }
-// }
