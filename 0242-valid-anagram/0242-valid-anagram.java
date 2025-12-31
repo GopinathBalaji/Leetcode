5815f1e76d5e607d1cbe38d95f01ab465ce14fd2
GopinathBalaji <70364682+GopinathBalaji@users.noreply.github.com>
2025-12-31 10:31:16 -0600

    Time: 16 ms (22.37%), Space: 46.8 MB (8.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,59 @@
+// Method 1: Without Sorting using HashMap
+/*
+*/
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        if(s.length() != t.length()){
-            return false;
-        }
+        if (s.length() != t.length()) return false;
 
         Map<Character, Integer> mapS = new HashMap<>();
         Map<Character, Integer> mapT = new HashMap<>();
 
-        for(char c : s.toCharArray()){
-            mapS.put(c, mapS.getOrDefault(c , 0) + 1);
+        for(char c: s.toCharArray()){
+            mapS.put(c, mapS.getOrDefault(c, 0) + 1);
         }
 
-        for(char c : t.toCharArray()){
-            mapT.put(c, mapT.getOrDefault(c , 0) + 1);
-        }        
+        for(char c: t.toCharArray()){
+            mapT.put(c, mapT.getOrDefault(c, 0) + 1);
+        }
 
-        return mapS.equals(mapT);
+        for(char c: s.toCharArray()){
+            int valS = mapS.get(c);
+
+            if(!mapT.containsKey(c) || mapT.get(c) != valS){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
+
+
+
+
+
+// Method 1.5: Similar using inbuilt function .equals() in HashMap
+/*
+*/
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+
+//         if(s.length() != t.length()){
+//             return false;
+//         }
+
+//         Map<Character, Integer> mapS = new HashMap<>();
+//         Map<Character, Integer> mapT = new HashMap<>();
+
+//         for(char c : s.toCharArray()){
+//             mapS.put(c, mapS.getOrDefault(c , 0) + 1);
+//         }
+
+//         for(char c : t.toCharArray()){
+//             mapT.put(c, mapT.getOrDefault(c , 0) + 1);
+//         }        
+
+//         return mapS.equals(mapT);
+//     }
+// }
