356a0e3c22ba3c3f16192d6716880f8cd5e57760
GopinathBalaji <70364682+GopinathBalaji@users.noreply.github.com>
2026-07-05 10:20:42 -0500

    Time: 0 ms (100.00%) | Memory: 10.9 MB (93.13%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+// Method 1: Iterative Method
+/*
+*/
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        if(root == nullptr){
+            return {};
+        }
+
+        vector<int> ans;
+
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty()){
+            TreeNode* curr = st.top();
+            st.pop();
+
+            ans.push_back(curr->val);
+
+            if(curr->left != nullptr){
+                st.push(curr->left);
+            }
+            if(curr->right != nullptr){
+                st.push(curr->right);
+            }
+        }
+
+        std::reverse(ans.begin(), ans.end());
+
+        return ans;        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
